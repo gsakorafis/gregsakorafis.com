43899492e54a0d086e0f983fa3f3c9164e8f06d6
Greg Sakorafis <gsakorafis@pinterest.com>
2020-01-06 13:04:16 -0800

    Remove debug

--- src/Analytics.js
+++ src/Analytics.js
@@ -5,7 +5,7 @@ import Analytics from 'react-router-ga';
 class PageAnalytics extends React.Component { 
   render() {
     return (
-      <Analytics id="G-2RZ2M5RYRM" debug>
+      <Analytics id="G-2RZ2M5RYRM">
         {this.props.children}
       </Analytics>
     );
